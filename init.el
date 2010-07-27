3a2d70477b2496e78f854249240bdc5a861619f8
Ershad K <ershad92@gmail.com>
2010-07-27 14:26:53 +0530

    First commit

---
+++ init.el
@@ -0,0 +1,24 @@
+(add-to-list 'load-path "/home/ershad/.emacs.d/")
+(require 'python)
+(require 'linum)
+(require 'boxquote)
+(define-key python-mode-map (kbd "C-m") 'newline-and-indent)
+(global-set-key (kbd "C-c p") (lambda () (interactive) (shell-command "git push origin master")))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(erc-notify-list nil)
+ '(erc-track-enable-keybindings t)
+ '(erc-track-shorten-cutoff 5)
+ '(erc-track-shorten-start 5)
+ '(erc-track-visibility t)
+ '(inhibit-startup-screen t))
+ 
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
