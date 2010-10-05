4262d95ad391aa030b70fb57501fbb24a1b9b23b
Ershad K <ershad92@gmail.com>
2010-10-05 22:49:33 +0530

    Minor additions.

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 (add-to-list 'load-path "/home/ershad/.emacs.d/")
 
+;;newline-and-indent in C++ mode, Thanks to ashawley - #emacs
+(add-hook 'c++-mode-hook 'ershad-c++-hook-fun)
+(defun ershad-c++-hook-fun ()
+  (local-set-key "\r" 'newline-and-indent))
+
+
+
 ;;------------------------------------------------------------------------------
 ;; require(s)
 
@@ -12,21 +19,21 @@
 ;;------------------------------------------------------------------------------
 ;; Jabber stuffs
 
-(add-to-list 'load-path "/home/ershad/.emacs.d/plugins/emacs-jabber/")
-(require 'jabber)
+;;(add-to-list 'load-path "/home/ershad/.emacs.d/plugins/emacs-jabber/")
+;;(require 'jabber)
 
-(global-set-key (kbd "S-<f9>") 'jabber-connect-all)
+;;(global-set-key (kbd "S-<f9>") 'jabber-connect-all)
 ;;(global-set-key (kbd "S-<f8>") 'switch-to-buffer "*-jabber-roster-*")
-(global-set-key     (kbd "S-<f8>") (lambda () (interactive)
-				   (switch-to-buffer "*-jabber-roster-*")))
+;;(global-set-key     (kbd "S-<f8>") (lambda () (interactive)
+;;				   (switch-to-buffer "*-jabber-roster-*")))
 
 ;;------------------------------------------------------------------------------
 ;; Twitter stuffs
 
-(autoload 'twitter-get-friends-timeline "twitter" nil t)
-(autoload 'twitter-status-edit "twitter" nil t)
-(global-set-key "\C-xt" 'twitter-get-friends-timeline)
-(add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
+;;(autoload 'twitter-get-friends-timeline "twitter" nil t)
+;;(autoload 'twitter-status-edit "twitter" nil t)
+;;(global-set-key "\C-xt" 'twitter-get-friends-timeline)
+;;(add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
 
 ;;------------------------------------------------------------------------------
 ;; Autoloads / Confs
@@ -44,11 +51,11 @@
 ;;------------------------------------------------------------------------------
 ;; Inactive(s)
 
-(add-to-list 'load-path
-	     "~/.emacs.d/plugins/")
-(require 'yasnippet) ;; not yasnippet-bundle
-(yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins/snippets")
+;;(add-to-list 'load-path
+;;	     "~/.emacs.d/plugins/")
+;;(require 'yasnippet) ;; not yasnippet-bundle
+;;(yas/initialize)
+;;(yas/load-directory "~/.emacs.d/plugins/snippets")
 
 ;;(setq jabber-account-list
 ;;    '(("ershad92@gmail.com" 
